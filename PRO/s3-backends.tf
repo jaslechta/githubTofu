60cd34c7c97ecf94144b8984331c8c29ba9a7977
jaslechta <121024739+jaslechta@users.noreply.github.com>
2026-04-09 09:15:53 +0200

    Update s3-backends.tf

--- PRO/s3-backends.tf
+++ PRO/s3-backends.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket  = "REPLACE_ME_TFSTATE_BUCKET"
+    bucket  = ""
     key     = "vault/PRO/terraform.tfstate"
-    region  = "eu-central-1"
+    region  = ""
     encrypt = true
   }
 }
